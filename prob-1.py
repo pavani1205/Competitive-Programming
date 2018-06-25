0fe4b11e09c8d9b28ff2044e2b4bd35a9236d985
pavani <perni.pavani@gmail.com>
2018-06-25 17:58:23 +0530

    first commit

---
+++ prob-1.py
@@ -0,0 +1,75 @@
+import sys
+def get_max_profit(stock_prices):
+    
+
+    # Calculate the max profit
+    min=stock_prices[0]
+    min1=stock_prices[0]
+    max=stock_prices[1]
+    for i in range(0,len(stock_prices)):
+        if(stock_prices[i]-min1>max-min and i!=0):
+           max=stock_prices[i]
+           min=min1
+        if(stock_prices[i]<min1):
+            min1=stock_prices[i]
+        
+
+    return max-min
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_price_goes_up_then_down(self):
+        actual = get_max_profit([1, 5, 3, 2])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_then_up(self):
+        actual = get_max_profit([7, 2, 8, 9])
+        expected = 7
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_up_all_day(self):
+        actual = get_max_profit([1, 6, 7, 9])
+        expected = 8
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_all_day(self):
+        actual = get_max_profit([9, 7, 4, 1])
+        expected = -2
+        self.assertEqual(actual, expected)
+
+    def test_price_stays_the_same_all_day(self):
+        actual = get_max_profit([1, 1, 1, 1])
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_one_price_raises_error(self):
+        with self.assertRaises(Exception):
+            get_max_profit([1])
+
+    def test_empty_list_raises_error(self):
+        with self.assertRaises(Exception):
+            get_max_profit([])
+
+unittest.main(verbosity=2)
